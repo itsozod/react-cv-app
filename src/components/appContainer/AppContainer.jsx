63d9e8acbd7773ec9629c7ac17258895d66eb765
Ozod <bukhorizodao@gmail.com>
2023-10-21 18:54:29 +0500

    set the props validation in AppContainer component

--- src/components/appContainer/AppContainer.jsx
+++ src/components/appContainer/AppContainer.jsx
@@ -1,4 +1,8 @@
 import styles from "./AppContainer.module.css";
+import PropTypes from 'prop-types';
 export default function AppContainer({children}) {
   return <div className={styles.app}>{children}</div>;
+}
+AppContainer.propTypes = {
+  children: PropTypes.node.isRequired
 }
